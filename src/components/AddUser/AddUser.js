ca2970cc027a6b355d3bb80aea267300ff58dfab
unknown <hasnainpriam925@gmail.com>
2022-03-15 23:49:57 +0600

    final commit

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React ,{ useRef } from 'react';
 
 const AddUser = () => {
+
+const nameRef=useRef();
+const emailRef=useRef();
+
+const handleAddUser=e=>{
+    e.preventDefault();
+    const name=nameRef.current.value;
+    const email=emailRef.current.value;
+
+const newUser={name,email};
+fetch('http://localhost:5000/users', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newUser)
+})
+.then(res=>res.json())
+.then(data=>{
+   if(data.insertedId){
+       alert("Data added")
+       console.log('Data new added.')
+       e.target.reset()
+   }
+})
+
+}
+
     return (
         <div>
             <h2>This is Add User</h2>
+            <form onSubmit={handleAddUser}>
+              <input type="text" ref={nameRef} placeholder='Name'/>
+              <input type="email" ref={emailRef} placeholder='Email'/>
+              <input type="submit" value='Add'/>
+            </form>
         </div>
     );
 };
--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import { useParams } from 'react-router-dom';
 
 const UpdateUser = () => {
+   const [user,setUser]=useState([]);
+   const {id}=useParams();
+   useEffect(()=>{
+       const url=`http://localhost:5000/users/${id}`;
+       fetch(url)
+       .then(res=>res.json())
+       .then(data=>setUser(data))
+   },[])
+  
+   const handleUpdate=e=>{
+    e.preventDefault();
+    const url=`http://localhost:5000/users/${id}`;
+    fetch(url, {
+        method: 'PUT',
+        headers: {
+           
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    }).then(res=>res.json())
+    .then(data=>
+        {
+            if(data.modifiedCount>0){
+                alert('Data updated successfully')
+                setUser({})
+            }
+        })
+    
+   }
+   const handleName=e=>{
+    const updatedName=e.target.value;
+    const updatedUser={ name:updatedName , email: user.email }
+    setUser(updatedUser)
+   }
+   const handleEmail=e=>{
+    const updatedEmail=e.target.value;
+    // const updatedUser={...user};
+    // updatedUser.email=updatedEmail;
+    const updatedUser={ email:updatedEmail , name:user.name }
+    setUser(updatedUser)
+   }
+
     return (
         <div>
             <h2>This is Update User</h2>
+            <h1>{user.name}</h1>
+            <h1>{user.email}</h1>
+
+            <form onSubmit={handleUpdate}>
+                <input type="text" onChange={handleName} value={user.name || ''} />
+                <input type="email" onChange={handleEmail} value={user.email || ''}/>
+                <input type="submit"  value='Submit'/>
+            </form>
         </div>
     );
 };
